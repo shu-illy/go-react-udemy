60fa57aedbc6d69b3b9bd212377afad95fafaedf
shu-illy <sh.illy.9117@gmail.com>
2024-01-04 03:06:05 +0900

    modify task model

--- model/task.go
+++ model/task.go
@@ -4,16 +4,16 @@ import "time"
 
 type Task struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title"`
+	Title     string    `json:"title" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	User      User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
-	UserID    uint      `json:"user_id" gorm:"not null"`
+	UserId    uint      `json:"user_id" gorm:"not null"`
 }
 
 type TaskResponse struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title"`
+	Title     string    `json:"title" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
